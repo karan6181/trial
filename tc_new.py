d149cbcaad2a84968b74f343f77afcbb1c6f65dd
Sharma <abhinav0301@gmail.com>
2019-04-29 15:25:56 -0700

    update model class

--- tc_new.py
+++ tc_new.py
@@ -1,25 +1,30 @@
-from automodel.text import TextClassifier
-from automodel.text.data import TextClassifierData
+# from automodel.text import TextClassifier
+# from automodel.text.data import TextClassifierData
+# from automodel.model import TextClassifierModel
 
-data = TextClassifierData(data_path="mydatapath")
-# provides feedback on the data
-# such as
-# number of classes, class:data sample ratio, suggestions for collecting more data
-# this will be useful to the user to get more high quality to further improve model
-#data.analyze()
+# data = TextClassifierData(data_path="mydatapath")
+# # provides feedback on the data
+# # such as
+# # number of classes, class:data sample ratio, suggestions for collecting more data
+# # this will be useful to the user to get more high quality to further improve model
+# #data.analyze()
+#
+# constraints = ModelConstraints().time_limit(1200).model_size(500).throughput(100)
+#
+# textClassifier = TextClassifer()
+#
+# textClassifier.fit(data,
+#                    metrics = ["loss", "perplexity"],
+#                    model_constraints = constraints,
+#                    resource_config = 'localhost',
+#                    model_generation_policy = TextClassifierPolicy.default())
+#
+# textClassifier.stats() # shows stats about train_loss, val_loss, etc.,
+#
+# textClassifer.evaluate(eval_data_path) # return stats
+#
+# textClassifier.save(path="./")
 
-constraints = ModelConstraints().time_limit(1200).model_size(500).throughput(100)
+from automodel.model import TextClassifierModel
 
-textClassifier = TextClassifer()
-
-textClassifier.fit(data,
-                   metrics = ["loss", "perplexity"],
-                   model_constraints = constraints,
-                   resource_config = 'localhost',
-                   model_generation_policy = TextClassifierPolicy.default())
-
-textClassifier.stats() # shows stats about train_loss, val_loss, etc.,
-
-textClassifer.evaluate(eval_data_path) # return stats
-
-textClassifier.save(path="./")
+text_model = TextClassifierModel()
