6f0c1178356b0f784d9afb20cfbeec19b83702ee
Karan Jariwala <karankjariwala@gmail.com>
2019-05-02 15:54:33 -0700

    Added Ray_Tune HPO integration with TextClassification model

--- tc_new.py
+++ tc_new.py
@@ -1,7 +1,7 @@
 from automodel.text import TextClassifier
 
 def main():
-    data_path = '/home/ubuntu/aclImdb'
+    data_path = '/Users/karjar/projects/automl/automodel_fit_abhinav/aclImdb'
     text_clas = TextClassifier()
     text_clas.fit(data_path)
     #print(len(text_data.train_dataloader))
